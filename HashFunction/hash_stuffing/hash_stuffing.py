0ba36f6a211f1026adfd623048e08e7450cff989
Nguyễn Huy San <144963803+san601@users.noreply.github.com>
2024-04-09 00:07:37 +0700

    Add some fun stuffs, brush my teeth and slip

--- HashFunction/hash_stuffing/hash_stuffing.py
+++ HashFunction/hash_stuffing/hash_stuffing.py
@@ -3,8 +3,8 @@ import json
 
 r = remote('socket.cryptohack.org', 13405, level='debug')
 
-block1 = b'\x01' * 64
-block2 = b'\x01' * 63
+block1 = b'\x01' * 32
+block2 = b'\x01' * 31
 block1 = block1.hex()
 block2 = block2.hex()
 payload = json.dumps({"m1": block1, "m2": block2}).encode()
