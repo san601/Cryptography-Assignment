6ff7580176b75e8c5dd0c6c960956bd74c0959d2
Nguyễn Huy San <144963803+san601@users.noreply.github.com>
2024-04-10 21:17:40 +0700

    Add some scripts, fix some bug and write something !fun

---
+++ AES/ctrime.py
@@ -0,0 +1,32 @@
+import requests
+import string
+
+
+def encrypt(plaintext):
+    plaintext = plaintext.encode().hex()
+    s = 'http://aes.cryptohack.org/ctrime/encrypt/' + plaintext + '/'
+    ciphertext = requests.get(s).json()['ciphertext']
+    return ciphertext
+
+
+alphabet = '}' + '!' + '_' + '@' + '?' + string.ascii_uppercase + string.digits + string.ascii_lowercase
+
+# plaintext = 'crypto{'
+plaintext = 'crypto{CRIME'
+current_length = len(encrypt(plaintext))
+
+while True:
+    flag = False
+    for char in alphabet:
+        ciphertext = encrypt(plaintext + char)
+        print(f"Trying {plaintext + char} with length {len(ciphertext)}")
+        if len(ciphertext) < current_length:
+            current_length = len(ciphertext)
+            print(f"Found character: {char}")
+            plaintext += char
+            flag = True
+            break
+        current_length = len(ciphertext)
+    if not flag:
+        print(plaintext)
+        break
